f1c4210e64b465d8f9631e0ecafb67d5761c8637
samuelribeiroo <samuelribeirodev@gmail.com>
2025-02-27 19:57:55 -0300

    feat: add header component

---
+++ frontend/src/app/components/header.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import {
+  CategoriesNavigation,
+  DesktopNavigation,
+  MobileMenu,
+} from "./ui/navigation";
+import useFetchCategories from "../hooks/useFetchCategories";
+
+export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const { categories } = useFetchCategories();
+
+  return (
+    <>
+      <header className="w-full border-b">
+        <DesktopNavigation />
+        <CategoriesNavigation categories={categories} />
+        <MobileMenu
+          openMenu={setIsOpen}
+          isOpen={isOpen}
+          categories={categories}
+        />
+      </header>
+    </>
+  );
+}
