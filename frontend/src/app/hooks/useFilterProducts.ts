1e2863b2aeff3fcada459cdfe5e67aae003da564
samuelribeiroo <samuelribeirodev@gmail.com>
2025-03-01 10:51:21 -0300

    feat: add custom hook filter

---
+++ frontend/src/app/hooks/useFilterProducts.ts
@@ -0,0 +1,59 @@
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useState, useEffect } from "react";
+import { Brand, Product } from "../@types";
+
+export default function useFilterProducts({
+  initialBrands,
+  initialProducts,
+}: {
+  initialBrands: Brand[];
+  initialProducts: Product[];
+}) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const [filteredProducts, setFilteredProducts] =
+    useState<Product[]>(initialProducts);
+
+  const [selectedBrandIds, setSelectedBrandIds] = useState<string[]>(
+    searchParams.getAll("brand")
+  );
+
+
+  useEffect(() => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.delete("brand");
+
+    selectedBrandIds.forEach((id) => params.append("brand", id));
+
+    router.replace(`${pathname}?${params.toString()}`);
+
+    const selectedProductsFromFilter = initialProducts.filter((product) =>
+      selectedBrandIds.includes(product.brandId)
+    );
+
+  
+    const newFilteredProducts: Product[] =
+      selectedBrandIds.length === 0
+        ? initialProducts
+        : selectedProductsFromFilter
+
+    setFilteredProducts(newFilteredProducts);
+  }, [selectedBrandIds]);
+
+  const handleBrandToggle = (brandId: string) => {
+    setSelectedBrandIds((prev) =>
+      prev.includes(brandId)
+        ? prev.filter((id) => id !== brandId)
+        : [...prev, brandId]
+    );
+  };
+
+  return {
+    initialBrands,
+    selectedBrandIds,
+    handleBrandToggle,
+    filteredProducts,
+  };
+}
