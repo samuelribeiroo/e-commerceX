758580cbeda950fca5deaaaff15548db48e3e572
samuelribeiroo <samuelribeirodev@gmail.com>
2025-04-04 11:45:19 -0300

    update: cache time one week now

--- frontend/src/app/categories/[id]/page.tsx
+++ frontend/src/app/categories/[id]/page.tsx
@@ -1,10 +1,10 @@
 import {  CACHE_TIME } from '@/app/data';
 import { notFound } from 'next/navigation';
 import { ProductCategoryView } from '../product-view';
+import { ParamsType } from '@/app/@types';
 
 export const dynamic = 'force-dynamic';
 
-type ParamsType = Promise<{ id: string }>
 
 export default async function ProductsCategoryPage(props: {  params: ParamsType }) {
   try {
@@ -13,7 +13,7 @@ export default async function ProductsCategoryPage(props: {  params: ParamsType 
       
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/categories/${id}`, {
       next: {
-        revalidate: CACHE_TIME.EIGHT_HOURS
+        revalidate: CACHE_TIME.ONE_WEEK
       }
     });
     
